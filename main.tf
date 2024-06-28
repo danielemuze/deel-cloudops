5da52a5224dd06294df98417045c9abfc3261ce3
danielemuze <danielemuze@gmail.com>
2024-06-28 22:49:03 +0100

    modified user_data

--- main.tf
+++ main.tf
@@ -141,25 +141,7 @@ resource "aws_launch_template" "web" {
   } 
   key_name          = aws_key_pair.deployer.key_name
 
-  user_data = <<-EOF
-                #!/bin/bash
-                sudo apt-get update -y
-                sudo apt-get upgrade -y
-                sudo apt-get install ca-certificates curl
-                sudo install -m 0755 -d /etc/apt/keyrings
-                sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-                sudo chmod a+r /etc/apt/keyrings/docker.asc
-                echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-                sudo apt-get update -y
-                sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-                sudo groupadd docker
-                sudo usermod -aG docker $USER
-                sudo reboot
-                newgrp docker
-                sudo systemctl enable docker.service
-                sudo systemctl enable containerd.service
-                docker run -d -p 80:80 yeasy/simple-web
-              EOF
+  user_data = filebase64("${path.module}/user_data.sh")
 }
 
 # Auto Scaling Group
---
+++ user_date.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo reboot
+newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+docker run -d -p 80:80 yeasy/simple-web
