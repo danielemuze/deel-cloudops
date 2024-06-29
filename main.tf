465a0a7bd1efadb6255912aa1da9b9f92ccc5cf4
danielemuze <danielemuze@gmail.com>
2024-06-29 01:03:13 +0100

    modified the main.tf file

--- main.tf
+++ main.tf
@@ -10,23 +10,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-module "network" {
-  source = "./network"
-}
-
-module "security" {
-  source = "./security"
-  vpc_id = module.network.vpc_id
-}
-
-module "compute" {
-  source            = "./compute"
-  vpc_id            = module.network.vpc_id
-  public_subnet_ids = module.network.public_subnet_ids
-  security_group_id = module.security.security_group_id
-}
-
-output "load_balancer_dns" {
-  value = module.compute.load_balancer_dns
-}
