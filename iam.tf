20e38f82847b87fc09a0eb220d5767f73527ffc9
danielemuze <danielemuze@gmail.com>
2024-07-01 00:06:37 +0100

    Errors corrected

--- iam.tf
+++ iam.tf
@@ -1,21 +1,25 @@
 resource "aws_iam_role" "ec2_role" {
-  name = "ec2_dynamodb_role"
+  name = "ec2_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
       }
-    }]
-  })
+    ]
+  }
+  EOF
 }
 
-resource "aws_iam_role_policy_attachment" "dynamodb_access" {
+resource "aws_iam_role_policy_attachment" "ec2_role_attachment" {
   role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,3 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_a_id" {
-  value = aws_subnet.public_a.id
-}
-
-output "public_subnet_b_id" {
-  value = aws_subnet.public_b.id
-}
-
-output "private_subnet_a_id" {
-  value = aws_subnet.private_a.id
-}
-
-output "private_subnet_b_id" {
-  value = aws_subnet.private_b.id
-}
-
-output "internet_gateway_id" {
-  value = aws_internet_gateway.main.id
-}
-
-output "public_route_table_id" {
-  value = aws_route_table.public.id
-}
-
-output "web_security_group_id" {
-  value = aws_security_group.web.id
-}
-
 output "elb_dns_name" {
   value = aws_lb.web.dns_name
 }
